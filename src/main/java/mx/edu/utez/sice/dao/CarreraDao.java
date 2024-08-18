54683736dbb4e258254f6c886d4dff5a93d089b0
Amador <20233tn077@utez.edu.mx>
2024-08-18 14:20:52 -0600

    Agregué estructura básica para empezar a agregar alumnos a grupos, así como grupos a un docente

--- src/main/java/mx/edu/utez/sice/dao/CarreraDao.java
+++ src/main/java/mx/edu/utez/sice/dao/CarreraDao.java
@@ -2,11 +2,13 @@ package mx.edu.utez.sice.dao;
 
 import mx.edu.utez.sice.model.Carrera;
 import mx.edu.utez.sice.model.DivisionAcademica;
+import mx.edu.utez.sice.model.Examen;
 import mx.edu.utez.sice.utils.DatabaseConnectionManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CarreraDao {
@@ -29,6 +31,27 @@ public class CarreraDao {
             e.printStackTrace();
         }
         return lista;
+    }
+
+    public ArrayList<Carrera> getAll(int id_division_academica) {
+        ArrayList<Carrera> carreras = new ArrayList<>();
+        String query = "select id_carrera, nombre_carrera, division_academica_id_division_academica from carrera join division_academica on division_academica_id_division_academica = id_division_academica where id_division_academica = ?;";
+        try {
+            Connection con = DatabaseConnectionManager.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, id_division_academica);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Carrera carrera = new Carrera();
+                carrera.setId_carrera(rs.getInt("id_carrera"));
+                carrera.setCarrera(rs.getString("nombre_carrera"));
+                carrera.setId_division_academica(rs.getInt("division_academica_id_division_academica"));
+                carreras.add(carrera);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return carreras;
     }
 
     public Carrera getById(int id) {
