6d493e1b95cc3e8942991999b54dc1721b2c158f
Elias-Manuel-Marquez-Bailon <20233tn068>
2024-08-22 06:54:40 -0600

    a

---
+++ src/main/java/mx/edu/utez/sice/dao/FiltroGrupoDao.java
@@ -0,0 +1,14 @@
+package mx.edu.utez.sice.dao;
+
+import mx.edu.utez.sice.model.Tabla;
+
+import java.util.ArrayList;
+
+public class FiltroGrupoDao {
+
+    /*public ArrayList<Tabla>getInfo (int grado, String grupo, int id_division_academica, int id_carrera) {
+        ArrayList<Tabla> tablas = new ArrayList<>();
+        String query = "";
+    }*/
+
+}
